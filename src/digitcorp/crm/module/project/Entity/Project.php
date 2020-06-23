ce143d185ee207a0c620e11749bfc3b10826cf92
tnteamdev3 <tn.teamdev3@gmail.com>
2020-06-23 03:31:37 -1100

    testt

--- src/digitcorp/crm/module/project/Entity/Project.php
+++ src/digitcorp/crm/module/project/Entity/Project.php
@@ -24,15 +24,6 @@ class Project
      */
     private $name_project;
 
-    /**
-     * @ORM\ManyToMany(targetEntity=App\digitcorp\crm\module\user\Entity\User::class, inversedBy="projects")
-     */
-    private $user;
-
-    /**
-     * @ORM\OneToMany(targetEntity=App\digitcorp\crm\module\task\Entity\Task::class, mappedBy="projet")
-     */
-    private $tasks;
 
    
 
@@ -59,62 +50,7 @@ class Project
         return $this;
     }
 
-    /**
-     * @return Collection|User[]
-     */
-    public function getUser(): Collection
-    {
-        return $this->user;
-    }
 
-    public function addUser(User $user): self
-    {
-        if (!$this->user->contains($user)) {
-            $this->user[] = $user;
-        }
-
-        return $this;
-    }
-
-    public function removeUser(User $user): self
-    {
-        if ($this->user->contains($user)) {
-            $this->user->removeElement($user);
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|Task[]
-     */
-    public function getTasks(): Collection
-    {
-        return $this->tasks;
-    }
-
-    public function addTask(Task $task): self
-    {
-        if (!$this->tasks->contains($task)) {
-            $this->tasks[] = $task;
-            $task->setProjet($this);
-        }
-
-        return $this;
-    }
-
-    public function removeTask(Task $task): self
-    {
-        if ($this->tasks->contains($task)) {
-            $this->tasks->removeElement($task);
-            // set the owning side to null (unless already changed)
-            if ($task->getProjet() === $this) {
-                $task->setProjet(null);
-            }
-        }
-
-        return $this;
-    }
 
           public function __toString(){
                
