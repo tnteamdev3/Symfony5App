0909b7c97d7dd3d8adc8f0b1127badd6d7810530
tnteamdev3 <tn.teamdev3@gmail.com>
2020-06-23 01:16:56 -1100

    kern

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\digitcorp\crm\module\;
+namespace App;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\Config\Resource\FileResource;
