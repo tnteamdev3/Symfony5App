ab5632342dfe9d07bbc42547050c620844d38ce5
tnteamdev3 <tn.teamdev3@gmail.com>
2020-06-23 00:45:32 -1100

    replace kernel.php

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\src;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\Config\Resource\FileResource;
