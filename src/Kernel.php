3639afc6f2e026323f2ad7d359fcb8ac8c33cbf3
tnteamdev3 <tn.teamdev3@gmail.com>
2020-06-23 00:52:33 -1100

    kernel.php

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\src;
+namespace App;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\Config\Resource\FileResource;
